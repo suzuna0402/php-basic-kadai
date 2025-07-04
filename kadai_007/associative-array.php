4d7b044e1c9529f2127352e9120221168f25a91f
suzuna0402 <suzuna_n8421@outlook.jp>
2025-07-04 20:21:33 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+<head>
+ <meta charset=”UTF-8″>
+ <title>【課題】PHP連想配列</title>
+</head>
+<body>
+  <p>
+    <?php
+      $personal_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+      print_r($personal_data);
+    ?>
+  </p>
+</body>
+</html>
